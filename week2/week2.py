ad1caadbf4518a3b88a31913e3f9c2b5a2b384c1
Andrew King <andy@givemexp.com>
2012-02-01 14:42:17 +1300

    week 2 start

---
+++ week2/week2.py
@@ -0,0 +1,19 @@
+import os
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+import week1.py
+
+class SearchPage(webapp.RequestHandler):
+	def get(self):
+		path = os.path.join(os.path.dirname(__file__), 'search.html')
+		self.response.out.write(template.render(path, {}))
+	def post(self):
+		path = os.path.join(os.path.dirname(__file__), 'result.html')
+		self.response.out.write(template.render(path, {}))
+
+
+app = webapp.WSGIApplication([('/', SearchPage)], debug=True)
+
+if __name__ == "__main__":
+	run_wsgi_app(app)
